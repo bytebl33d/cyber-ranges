1a603632124c729375cf01aa2cada638d5a53875
bytebl33d <lars.vandergaast@protonmail.com>
2025-12-29 16:56:08 +0100

    Fix: sliver beacon not running in user context

--- ad/PUPPET/files/bot.ps1
+++ ad/PUPPET/files/bot.ps1
@@ -1,18 +1,11 @@
-# 1. Define the process name (usually the filename without .exe)
 $processName = "sliver_beacon"
 $filePath = "C:\Files\sliver_beacon.exe"
 
-# 2. Check if the process is already running
+# Check if the process is already running
 if (Get-Process -Name $processName -ErrorAction SilentlyContinue) {
     Write-Output "Beacon is already running. Exiting."
 }
 else {
     Write-Output "Beacon not found. Starting now..."
-    
-    # 3. Setup Credentials
-    $pass = ConvertTo-SecureString 'Legend4_ryS4n1nFTW' -AsPlainText -Force
-    $creds = New-Object System.Management.Automation.PSCredential ('PUPPET\bruce.smith', $pass)
-
-    # 4. Start the process
     Start-Process -FilePath $filePath -Credential $creds -WindowStyle Hidden
 }
